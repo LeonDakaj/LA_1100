ce13f17c014ec85069e4f0833e3f29f1fe227f59
LeonDakaj <89085609+LeonDakaj@users.noreply.github.com>
2022-09-08 12:09:59 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿namespace Guess_MyNumber_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("╔═══╗                     ╔═╗╔═╗     ╔═╗ ╔╗        ╔╗              ╔═══╗             \r\n║╔═╗║                     ║║╚╝║║     ║║╚╗║║        ║║              ║╔═╗║             \r\n║║ ╚╝╔╗╔╗╔══╗╔══╗╔══╗     ║╔╗╔╗║╔╗ ╔╗║╔╗╚╝║╔╗╔╗╔╗╔╗║╚═╗╔══╗╔═╗     ║║ ╚╝╔══╗ ╔╗╔╗╔══╗\r\n║║╔═╗║║║║║╔╗║║══╣║══╣╔═══╗║║║║║║║║ ║║║║╚╗║║║║║║║╚╝║║╔╗║║╔╗║║╔╝╔═══╗║║╔═╗╚ ╗║ ║╚╝║║╔╗║\r\n║╚╩═║║╚╝║║║═╣╠══║╠══║╚═══╝║║║║║║║╚═╝║║║ ║║║║╚╝║║║║║║╚╝║║║═╣║║ ╚═══╝║╚╩═║║╚╝╚╗║║║║║║═╣\r\n╚═══╝╚══╝╚══╝╚══╝╚══╝     ╚╝╚╝╚╝╚═╗╔╝╚╝ ╚═╝╚══╝╚╩╩╝╚══╝╚══╝╚╝      ╚═══╝╚═══╝╚╩╩╝╚══╝\r\n                                ╔═╝║                                                 \r\n                                ╚══╝                                                 \r\n");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("In diesem Spiel ist es dein Ziel mit möglichst wenig versuche,meine Zufälig generierte Zahl von 1-100 herauszufinden");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            NumberGuesser:
+            Random rnd = new Random();
+            int randNumb = rnd.Next(1, 100);
+            int guessNumb;
+            int versueche = 0;
+            bool zahlGefunden = false;
+            bool trueFalse = false;
+
+            while (!zahlGefunden || trueFalse)
+            {
+
+                try
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write("Tippe eine Zahl zwischen 1-100: ");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    guessNumb = Convert.ToInt32(Console.ReadLine());
+
+
+                    if (guessNumb > 100 || guessNumb < 1) // 1-100
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("Bitte eine Zahl im Rahmen von 1-100 raten!");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("--------------------------------");
+                        Console.WriteLine("");
+
+                    }
+
+                    else if (guessNumb > randNumb) // Zu hoch geraten
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("Zu hoch geraten. Versuch mal TIEFER");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("--------------------------------");
+                        Console.WriteLine("");
+                        versueche++;
+                    }
+
+                    else if (guessNumb < randNumb) // Zu tief geraten
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("Zu tief. Versuch mal HÖHER");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("--------------------------------");
+                        Console.WriteLine("");
+                        versueche++;
+                    }
+
+                    else if (guessNumb == randNumb) // Richtige Zahl eratet
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("--------------------------------");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Bravo! Du hast meine Zahl eraten!");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        versueche++;
+
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("Du hattest insegsamt " + versueche + " versuche");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        zahlGefunden = true;
+                        Console.WriteLine("--------------------------------");
+                        Console.WriteLine("");
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write("Willst du wieder Spielen? [True/False]: ");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        trueFalse = Convert.ToBoolean(Console.ReadLine());
+
+                        if (trueFalse)
+                        {
+                            goto NumberGuesser;
+                        }
+                    }
+
+                }
+                catch // Keine Zahl eingegeben
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("UNGÜLTIGE Eingabe. Bitte eine Zahl im Rahmen von 1-100 raten!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("--------------------------------");
+                    Console.WriteLine("");
+                }
+            }
+        }
+    }
+}
